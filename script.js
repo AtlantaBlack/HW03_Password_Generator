9083cfcdeb75d21a5abf3ca56d379a00b5210d82
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-03-05 22:46:59 +1100

    added confirm inputs for lowercase, uppercase, num, and special char sets

--- script.js
+++ script.js
@@ -3,36 +3,95 @@ var generateBtn = document.querySelector("#generate");
 
 
 // set the four arrays needed
-const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const uppercase = lowercase.map(function (e) {
-    return e.toUpperCase();
-});
-const numerals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const specialChars = [']', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?'];
+// const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// const upperCase = lowerCase.map(function (e) {
+//     return e.toUpperCase();
+// });
+// const numerals = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// const specialChars = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?'];
 
+
+const passOptions = {
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    num: "0123456789",
+    special: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+}
+
+let userPass = "";
+const passCharSet = [];
+    
 
 // code for generating the password starts here
 function generatePassword() {
-        
+
     // create the password Length
     var userInput = prompt("How many characters long?\n(Enter a number between 8 and 128)");
         // exit out if user presses cancel
         if (userInput === null) {
             return;
-        };
+        }
 
-        // if user input is empty, not a number, under 8, or over 128, send up an alert to redo
+    // if user input is empty, not a number, under 8, or over 128, send up an alert to redo
     var passLength = parseInt(userInput);
         if (!userInput || isNaN(userInput) || passLength < 8 || passLength > 128) {
             alert("Please choose a number between 8 and 128.");
-            generatePassword();
-        };
+            getPasswordLength();
+        }
 
-    var userLCase = confirm("Do you want to use lowercase letters?");
-        alert(userLCase);
+    // ask user if they want lowercase, uppercase, numeric or special characters
 
+    // If yes, add corresponding object string to userPass variable. 
 
-};
+    // At the same time, add one random character from that string to the passCharSet array (for shuffling later). This is to make sure final password contains at least one of each selected character type
+    let getLowerCase = window.confirm("Password to contain lowercase letters?");
+        if (getLowerCase) {
+            userPass += passOptions.lowercase;
+            passCharSet.push(getRandomChar(passOptions.lowercase));
+        } 
+
+        console.log(userPass);
+        console.log(passCharSet);
+
+    let getUpperCase = window.confirm("Password to contain UPPERCASE letters?")
+        if (getUpperCase) {
+            userPass += passOptions.uppercase;
+            passCharSet.push(getRandomChar(passOptions.uppercase));
+        }
+
+        console.log(userPass);
+        console.log(passCharSet);
+
+    let getNumerals = window.confirm("Password to contain numeric numbers? (0-9)")
+        if (getNumerals) {
+            userPass += passOptions.num;
+            passCharSet.push(getRandomChar(passOptions.num));
+        }
+
+        console.log(userPass);
+        console.log(passCharSet);
+
+    let getSpecials = window.confirm("Password to contain special characters? (eg. ? ! @ $)")
+        if (getSpecials) {
+            userPass += passOptions.special;
+            passCharSet.push(getRandomChar(passOptions.special));
+        }
+
+        console.log(userPass);
+        console.log(passCharSet);
+
+    if (!passCharSet.length >= 1) {
+        alert("Uh oh, something's amiss!\n\nPlease choose at least one (1) character type to include in your password.");
+    }
+        
+
+function getRandomChar(fromString){
+    return fromString[Math.floor(Math.random() * fromString.length)];
+    }
+
+}
+
+// console.log(stringGen(passLength));
 
 
 // Write password to the #password input
